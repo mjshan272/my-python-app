6d7e58a5699388e797d00d43549161572971351e
mjshan272 <mjshan.272@gmail.com>
2025-11-28 18:21:18 +0000

    Add all files to repository

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python running in Kubernetes!"
+    return "Hello from Python running in Kubernetes! MJ is uprading to CI CD DEvops"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
